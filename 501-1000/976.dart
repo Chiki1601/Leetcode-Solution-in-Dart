1ed62486bb24559c7afda99d787b2a175e61d558
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2022-10-12 15:41:33 +0530

    Create 976.dart

---
+++ 501-1000/976.dart
@@ -0,0 +1,9 @@
+class Solution {
+  int largestPerimeter(List<int> nums) {
+    nums.sort();
+    for (int i = nums.length - 1; i > 1; --i)
+      if (nums[i] < nums[i - 1] + nums[i - 2])
+        return nums[i] + nums[i - 1] + nums[i - 2];
+    return 0;
+  }
+}
