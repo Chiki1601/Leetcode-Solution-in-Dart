120372098ee9bf2ccff738a3ca4b5b1a0fdbb4b5
Miss Pooja Anilkumar Patel <85725008+Chiki1601@users.noreply.github.com>
2023-02-06 15:18:29 +0530

    Update 1.dart

--- 1-500/1.dart
+++ 1-500/1.dart
@@ -1,15 +1,18 @@
 class Solution {
-    List<int> twoSum(List<int> nums, int target) {
-      List<int> answer = [];
-      for(int i = 0; i< nums.length; i++){
-         for(int j = i+1; j< nums.length; j++){
-             if((nums[i]+nums[j]) == target){
-                 answer.add(i);
-                 answer.add(j);
-             }
-         }
-      }
-      return answer;
+  List<int> twoSum(List<int> nums, int target) {
+      List<int> possitions = [];
+        if(nums.length == 2){
+            possitions.addAll([0,1]);
+        
+        }
+       else{ for(int i =0;i<nums.length;i++){
+            for(int j = i+1;j<nums.length;j++){
+                if(nums[i]+nums[j]==target){
+                     possitions.addAll([i,j]);
+                                      
+                }
+            }
+        }}
+        return possitions;
   }
-}
 }
